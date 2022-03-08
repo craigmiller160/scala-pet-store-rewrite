907518e3113a8efb6ba45d4b62fde5a23328dcb6
Craig Miller <craigmiller160@gmail.com>
2022-03-08 15:51:21 -0500

    Even more progress

--- src/main/scala/io/craigmiller160/scalapetstore/Runner.scala
+++ src/main/scala/io/craigmiller160/scalapetstore/Runner.scala
@@ -1,16 +1,24 @@
 package io.craigmiller160.scalapetstore
 
 import io.craigmiller160.scalapetstore.config._
-import io.circe._
+import io.circe.parser
+import io.circe.config.{parser => configParser}
 import io.craigmiller160.scalapetstore.config.AppConfig
 
 import java.io.{BufferedReader, InputStreamReader}
 import java.util.stream.Collectors
 
 object Runner extends App {
+  println("V1 Approach")
   val stream = getClass.getClassLoader.getResourceAsStream("application.json")
   val text = new BufferedReader(new InputStreamReader(stream)).lines().collect(Collectors.joining("\n"))
   parser.parse(text)
     .flatMap(_.as[AppConfig])
     .foreach(config => println(config))
+
+  println("V2 Approach")
+  configParser.decodePath[AppConfig]("petstore") match {
+    case Right(value) => println(value)
+    case Left(ex) => ex.printStackTrace()
+  }
 }
