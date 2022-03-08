309d3b3f02ff53b4b7fbef1073e3d4f6c89e3730
Craig Miller <craigmiller160@gmail.com>
2022-03-08 15:58:47 -0500

    Got application to start

--- src/main/scala/io/craigmiller160/scalapetstore/Runner.scala
+++ src/main/scala/io/craigmiller160/scalapetstore/Runner.scala
@@ -5,16 +5,10 @@ import cats.effect.{ExitCode, IO, IOApp, Resource}
 import io.craigmiller160.scalapetstore.config._
 import io.circe.config.{parser => configParser}
 import io.craigmiller160.scalapetstore.config.AppConfig
-import cats.effect._
 
 object Runner extends IOApp {
-  configParser.decodePath[AppConfig]("petstore") match {
-    case Right(value) => println(value)
-    case Left(ex) => ex.printStackTrace()
-  }
-
   override def run(args: List[String]): IO[ExitCode] =
-    createServer[IO].use(_ => IO.never).as(ExitCode.Success)
+    createServer[IO].use(config => IO.println(config)).as(ExitCode.Success)
 
   private def createServer[F[_]: Sync]: Resource[F, AppConfig] =
     for {
