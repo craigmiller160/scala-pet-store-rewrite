91375ad093e9135012e05240f94a5d3da6f0db7d
Craig Miller <craigmiller160@gmail.com>
2022-03-08 16:57:22 -0500

    Got my first HKT scenario working

--- src/main/scala/io/craigmiller160/scalapetstore/Temp.scala
+++ src/main/scala/io/craigmiller160/scalapetstore/Temp.scala
@@ -5,9 +5,9 @@ object Temp extends App {
     def map[A,B](fa: F[A])(f: A => B): F[B]
   }
 
-//  implicit val fooOption = new Foo[Option] {
-//    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = ???
-//  }
+  implicit val fooOption = new Foo[Option] {
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
+  }
 
   val list: List[String] = List("World")
   val option: Option[String] = Option("Hello")
@@ -16,13 +16,14 @@ object Temp extends App {
   practice[List](list)
 
   println("Phase 2")
-  practice[Option](option)
+  val phase2Result = practiceFoo(option)
+  println(phase2Result)
 
   def practice[F[_]](value: F[_]): Unit = {
     println(value)
   }
 
-  def practiceFoo[F[_]: Foo](value: F[_]): Unit = {
-    println(value)
+  def practiceFoo[F[_]: Foo, A](value: F[A])(implicit foo: Foo[F]): F[String] = {
+    foo.map(value) { a => s"$a Stuff" }
   }
 }
